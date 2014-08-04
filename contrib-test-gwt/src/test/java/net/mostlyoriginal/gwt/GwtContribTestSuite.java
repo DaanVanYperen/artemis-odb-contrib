e865d43d70fd1be3d6dbd3abf075408ece335f73
Daan <daan@mostlyoriginal.net>
2014-08-04 22:42:58 +0200

    Add GWT testing support. It works! What. huh. D:

---
+++ contrib-test-gwt/src/test/java/net/mostlyoriginal/gwt/GwtContribTestSuite.java
@@ -0,0 +1,19 @@
+package net.mostlyoriginal.gwt;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ * Runs contribution tests.
+ *
+ * @author Daan van Yperen
+ */
+public class GwtContribTestSuite extends TestCase {
+
+    public static Test suite() {
+      GWTTestSuite suite = new GWTTestSuite("Tests for artemis-odb-contrib.");
+      suite.addTestSuite(MyTest.class);
+      return suite;
+    }
+}
---
+++ contrib-test-gwt/src/test/java/net/mostlyoriginal/gwt/MyTest.java
@@ -0,0 +1,18 @@
+package net.mostlyoriginal.gwt;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * @author Daan van Yperen
+ */
+public class MyTest extends GWTTestCase {
+
+    @Override
+    public String getModuleName() {
+        return "net.mostlyoriginal.ContribTest";
+    }
+
+    public void test_1() throws Exception {
+        fail();
+    }
+}
