b83b456cb632be16cef38f857cacd8364abcaeda
Daan <daan@mostlyoriginal.net>
2014-08-07 20:10:10 +0200

    Add network entity subscription manager and test.

---
+++ contrib-network/src/main/java/net/mostlyoriginal/api/network/DeltaSubscriptionManager.java
@@ -0,0 +1,9 @@
+package net.mostlyoriginal.api.network;
+
+/**
+ * Subscription manager for network connections.
+ *
+ * @author Daan van Yperen
+ */
+public class DeltaSubscriptionManager extends SubscriptionManager<Integer> {
+}
