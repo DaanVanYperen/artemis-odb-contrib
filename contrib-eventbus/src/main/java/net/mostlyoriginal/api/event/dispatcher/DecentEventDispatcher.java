de24448a52a64c4abd6370410c76123ff2611df4
DaanVanYperen <daan@mostlyoriginal.net>
2014-09-14 14:46:36 +0200

    Add scaffolding for decent event dispatcher.

---
+++ contrib-eventbus/src/main/java/net/mostlyoriginal/api/event/dispatcher/DecentEventDispatcher.java
@@ -0,0 +1,19 @@
+package net.mostlyoriginal.api.event.dispatcher;
+
+import net.mostlyoriginal.api.event.common.Event;
+import net.mostlyoriginal.api.event.common.EventDispatchStrategy;
+import net.mostlyoriginal.api.event.common.EventListener;
+
+/**
+ * Created by Daan on 14-9-2014.
+ */
+public class DecentEventDispatcher implements EventDispatchStrategy {
+
+	@Override
+	public void register(EventListener listener) {
+	}
+
+	@Override
+	public void dispatch(Event event) {
+	}
+}
