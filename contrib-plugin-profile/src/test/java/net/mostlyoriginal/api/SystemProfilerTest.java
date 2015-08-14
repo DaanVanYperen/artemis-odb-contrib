9507be82f0bf9d4c34b2ce679cc3fb39060bd6b7
PiotrJ <me@piotrjastrzebski.io>
2015-08-14 19:01:21 +0200

    added new modules with profiler for systems

---
+++ contrib-plugin-profile/src/test/java/net/mostlyoriginal/api/SystemProfilerTest.java
@@ -0,0 +1,22 @@
+package net.mostlyoriginal.api;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author piotr-j
+ */
+public class SystemProfilerTest {
+	/*
+		stuff to test
+		ProfilerInvocationStrategy
+		add profilers to systems, dont add to existing, process working, profilers not getting mixed
+
+		SystemProfiler
+		all the things i guess
+	 */
+	@Test
+	public void test_things () {
+
+	}
+}
