eee9b81f2ab7fdb0f02d4ed3e46415100b57c89c
DaanVanYperen <daan@mostlyoriginal.net>
2015-08-30 20:10:39 +0200

    Add documentation to Mirrorable.

--- contrib-core/src/main/java/net/mostlyoriginal/api/component/common/Mirrorable.java
+++ contrib-core/src/main/java/net/mostlyoriginal/api/component/common/Mirrorable.java
@@ -3,6 +3,8 @@ package net.mostlyoriginal.api.component.common;
 import com.artemis.Component;
 
 /**
+ * Support mirroring state of another component.
+ *
  * @author Daan van Yperen
  */
 public interface Mirrorable<T extends Component & Mirrorable> {
