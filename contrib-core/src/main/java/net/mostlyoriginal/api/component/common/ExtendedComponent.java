866c4f2e85fdbce2bbdd747a6e976298c413d9ca
DaanVanYperen <daan@mostlyoriginal.net>
2015-08-30 10:38:03 +0200

    Add documentation.

--- contrib-core/src/main/java/net/mostlyoriginal/api/component/common/ExtendedComponent.java
+++ contrib-core/src/main/java/net/mostlyoriginal/api/component/common/ExtendedComponent.java
@@ -8,9 +8,19 @@ import java.io.Serializable;
 /**
  * Blueprint for extended component.
  *
+ * Rich components:
+ * - Are pooled.
+ * - Can mirror state of another component.
+ *
  * @author Daan van Yperen
  */
 public abstract class ExtendedComponent<T extends Component> extends PooledComponent implements Serializable {
 
+	/**
+	 * Mirror state of passed component.
+	 *
+	 * @param t component to mirror
+	 * @return {@code this}
+	 */
 	public abstract T set(T t);
 }
